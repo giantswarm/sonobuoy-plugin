3e11b0932de175c84810f2d0b6711134a35b27f2
Christian Bianchi <whites11@gmail.com>
2021-01-12 17:26:47 +0100

    wip

---
+++ tests/azure-delete/azure_delete_test.go
@@ -0,0 +1,31 @@
+package ingress
+
+import (
+	"testing"
+)
+
+func Test_AzureDelete(t *testing.T) {
+	//var err error
+	//
+	//ctx := context.Background()
+	//
+	//cpCtrlClient, err := ctrlclient.CreateCPCtrlClient(ctx)
+	//if err != nil {
+	//	t.Fatalf("error creating CP k8s client: %v", err)
+	//}
+	//
+	//logger, err := micrologger.New(micrologger.Config{})
+	//if err != nil {
+	//	t.Fatal(err)
+	//}
+	//
+	//clusterID, exists := os.LookupEnv("CLUSTER_ID")
+	//if !exists {
+	//	t.Fatal("missing CLUSTER_ID environment variable")
+	//}
+	//
+	//cpKubeConfig, err := ctrlclient.GetCPKubeConfig(ctx)
+	//if err != nil {
+	//	t.Fatal(err)
+	//}
+}
