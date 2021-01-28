291085da72471b816519288e7749b29811b48c7b
Jose Armesto <github@armesto.net>
2021-01-28 12:19:33 +0100

    Output

--- run_go_test.sh
+++ run_go_test.sh
@@ -20,9 +20,13 @@ mkdir "${results_dir}" || true
 echo "Report will be saved to ${junit_report_file}"
 
 # Run all tests.
-go test -v -timeout 99999s ./tests/... 2>&1 | go-junit-report > "${results_dir}/report.xml"
+go test -v -timeout 99999s ./tests/availabilityzones 2>&1 | go-junit-report > "${results_dir}/report.xml"
 
 # Run the deletion test (tiers down the cluster).
 go test -v -timeout 99999s ./deletiontests/... 2>&1 | go-junit-report > "${results_dir}/deletiontests.xml"
 
 jrm "${junit_report_file}" "${results_dir}/report.xml" "${results_dir}/deletiontests.xml"
+
+ls -lah "${results_dir}"
+
+cat "${junit_report_file}"
