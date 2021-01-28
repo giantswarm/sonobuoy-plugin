b1aabefb7d685b5e05264c60a606e1e9c70faee6
Jose Armesto <github@armesto.net>
2021-01-28 13:29:59 +0100

    Generate junit report so that sonobuoy parses the result (#20)

--- run_go_test.sh
+++ run_go_test.sh
@@ -1,14 +1,13 @@
 #!/usr/bin/env bash
 
-set -eo pipefail
-
 results_dir="${RESULTS_DIR:-/tmp/results}"
+junit_report_file="${results_dir}/combined-report.xml"
 
 # saveResults prepares the results for handoff to the Sonobuoy worker.
-# See: https://github.com/vmware-tanzu/sonobuoy/blob/master/docs/plugins.md
+# See: https://github.com/vmware-tanzu/sonobuoy/blob/master/site/docs/master/plugins.md
 saveResults() {
-	# Signal to the worker that we are done and where to find the results.
-	printf ${results_dir}/out > "${results_dir}/done"
+  # Signal to the worker that we are done and where to find the results.
+  printf ${junit_report_file} >"${results_dir}/done"
 }
 
 # Ensure that we tell the Sonobuoy worker we are done regardless of results.
@@ -17,7 +16,9 @@ trap saveResults EXIT
 mkdir "${results_dir}" || true
 
 # Run all tests.
-go test -v -timeout 99999s ./tests/... 2>&1 | tee -a "${results_dir}/out"
+go test -v -timeout 99999s ./tests/... 2>&1 | go-junit-report >"${results_dir}/report.xml"
 
 # Run the deletion test (tiers down the cluster).
-go test -v -timeout 99999s ./deletiontests/... 2>&1 | tee -a "${results_dir}/out"
+go test -v -timeout 99999s ./deletiontests/... 2>&1 | go-junit-report >"${results_dir}/deletiontests.xml"
+
+jrm "${junit_report_file}" "${results_dir}/report.xml" "${results_dir}/deletiontests.xml"
