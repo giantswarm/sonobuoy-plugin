b6f737ccc900e32cd326945e282d204a12eb2feb
Jose Armesto <github@armesto.net>
2021-01-28 13:19:12 +0100

    Clean

--- run_go_test.sh
+++ run_go_test.sh
@@ -6,8 +6,8 @@ junit_report_file="${results_dir}/combined-report.xml"
 # saveResults prepares the results for handoff to the Sonobuoy worker.
 # See: https://github.com/vmware-tanzu/sonobuoy/blob/master/site/docs/master/plugins.md
 saveResults() {
-	# Signal to the worker that we are done and where to find the results.
-	printf ${junit_report_file} > "${results_dir}/done"
+  # Signal to the worker that we are done and where to find the results.
+  printf ${junit_report_file} >"${results_dir}/done"
 }
 
 # Ensure that we tell the Sonobuoy worker we are done regardless of results.
@@ -15,20 +15,10 @@ trap saveResults EXIT
 
 mkdir "${results_dir}" || true
 
-echo "Report will be saved to ${junit_report_file}"
-
 # Run all tests.
-go test -v -timeout 99999s ./tests/availabilityzones 2>&1 | go-junit-report > "${results_dir}/report.xml"
-
-ls -lah "${results_dir}"
+go test -v -timeout 99999s ./tests/... 2>&1 | go-junit-report >"${results_dir}/report.xml"
 
 # Run the deletion test (tiers down the cluster).
-go test -v -timeout 99999s ./deletiontests/... 2>&1 | go-junit-report > "${results_dir}/deletiontests.xml"
-
-ls -lah "${results_dir}"
+go test -v -timeout 99999s ./deletiontests/... 2>&1 | go-junit-report >"${results_dir}/deletiontests.xml"
 
 jrm "${junit_report_file}" "${results_dir}/report.xml" "${results_dir}/deletiontests.xml"
-
-ls -lah "${results_dir}"
-
-cat "${junit_report_file}"
