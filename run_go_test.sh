ca3b87aee2730984b16413f88f0582d8f20e15e7
Jose Armesto <github@armesto.net>
2021-01-28 09:30:10 +0100

    Use right path for junit report

--- run_go_test.sh
+++ run_go_test.sh
@@ -6,10 +6,10 @@ results_dir="${RESULTS_DIR:-/tmp/results}"
 junit_report_file="${results_dir}/report.xml"
 
 # saveResults prepares the results for handoff to the Sonobuoy worker.
-# See: https://github.com/vmware-tanzu/sonobuoy/blob/master/docs/plugins.md
+# See: https://github.com/vmware-tanzu/sonobuoy/blob/master/site/docs/master/plugins.md
 saveResults() {
 	# Signal to the worker that we are done and where to find the results.
-	printf ${results_dir}/out > "${results_dir}/done"
+	printf ${junit_report_file > "${results_dir}/done"
 }
 
 # Ensure that we tell the Sonobuoy worker we are done regardless of results.
