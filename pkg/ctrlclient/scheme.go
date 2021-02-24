76554699cb7ff5437f15752620d609576996eae5
Christian Bianchi <whites11@gmail.com>
2021-02-24 11:02:46 +0100

    renamed import alias

--- pkg/ctrlclient/scheme.go
+++ pkg/ctrlclient/scheme.go
@@ -3,7 +3,7 @@ package ctrlclient
 import (
 	appv1alpha1 "github.com/giantswarm/apiextensions/v3/pkg/apis/application/v1alpha1"
 	corev1alpha1 "github.com/giantswarm/apiextensions/v3/pkg/apis/core/v1alpha1"
-	"github.com/giantswarm/apiextensions/v3/pkg/apis/release/v1alpha1"
+	releasev1alpha1 "github.com/giantswarm/apiextensions/v3/pkg/apis/release/v1alpha1"
 	appsv1 "k8s.io/api/apps/v1"
 	corev1 "k8s.io/api/core/v1"
 	"k8s.io/apiextensions-apiserver/pkg/apis/apiextensions"
@@ -26,7 +26,7 @@ func init() {
 		appsv1.AddToScheme,
 		corev1.AddToScheme,
 		corev1alpha1.AddToScheme,
-		v1alpha1.AddToScheme,
+		releasev1alpha1.AddToScheme,
 		appv1alpha1.AddToScheme,
 	}
 	err := schemeBuilder.AddToScheme(Scheme)
