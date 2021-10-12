709c1aac2de371f28a71e67e73f026d9bec023af
Christian Bianchi <whites11@gmail.com>
2021-10-12 17:44:29 +0200

    capz support

--- azuremachinepool_test.go
+++ azuremachinepool_test.go
@@ -4,8 +4,10 @@ import (
 	"context"
 	"os"
 	"testing"
+	"time"
 
 	"github.com/giantswarm/apiextensions/v3/pkg/label"
+	"github.com/giantswarm/backoff"
 	"github.com/giantswarm/microerror"
 	"github.com/giantswarm/micrologger"
 	capz "sigs.k8s.io/cluster-api-provider-azure/api/v1alpha4"
@@ -15,6 +17,7 @@ import (
 	"github.com/giantswarm/sonobuoy-plugin/v5/pkg/assert"
 	"github.com/giantswarm/sonobuoy-plugin/v5/pkg/capiutil"
 	"github.com/giantswarm/sonobuoy-plugin/v5/pkg/ctrlclient"
+	"github.com/giantswarm/sonobuoy-plugin/v5/pkg/key"
 	"github.com/giantswarm/sonobuoy-plugin/v5/pkg/provider"
 )
 
@@ -84,7 +87,32 @@ func Test_AzureMachinePoolCR(t *testing.T) {
 		// Check if 'giantswarm.io/machine-pool' label is set
 		assert.LabelIsSet(t, &amp, label.MachinePool)
 
-		capiutil.WaitForCondition(t, ctx, logger, &amp, capi.ReadyCondition, capiconditions.IsTrue, azureMachinePoolGetter)
+		if key.IsCapiRelease(amp.Labels["release.giantswarm.io/version"]) {
+			// CAPZ cluster.
+			o := func() error {
+				machinePool, err := capiutil.FindAzureMachinePool(ctx, cpCtrlClient, amp.Name)
+				if err != nil {
+					return microerror.Maskf(executionFailedError, "error finding AzureMachinePool %s: %s", amp.Name, microerror.JSON(err))
+				}
+
+				if !machinePool.Status.Ready {
+					return microerror.Maskf(unexpectedValueError, "expected %q AzureMachinePool's status.ready field to be true, was false", amp.Name)
+				}
+
+				return nil
+			}
+
+			b := backoff.NewExponential(20*time.Minute, backoff.LongMaxInterval)
+			n := backoff.NewNotifier(logger, ctx)
+			err := backoff.RetryNotify(o, b, n)
+			if err != nil {
+				t.Fatalf(
+					"error while waiting for azure machine pool to become ready")
+			}
+		} else {
+			// GS cluster.
+			capiutil.WaitForCondition(t, ctx, logger, &amp, capi.ReadyCondition, capiconditions.IsTrue, azureMachinePoolGetter)
+		}
 
 		machinePool, err := capiutil.FindMachinePool(ctx, cpCtrlClient, amp.Name)
 		if err != nil {
--- machinepool_test.go
+++ machinepool_test.go
@@ -11,11 +11,14 @@ import (
 	"github.com/giantswarm/conditions/pkg/conditions"
 	"github.com/giantswarm/microerror"
 	"github.com/giantswarm/micrologger"
+	"sigs.k8s.io/cluster-api-provider-azure/exp/api/v1alpha4"
+	capi "sigs.k8s.io/cluster-api/api/v1alpha4"
+	capiconditions "sigs.k8s.io/cluster-api/util/conditions"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+
 	"github.com/giantswarm/sonobuoy-plugin/v5/pkg/assert"
 	"github.com/giantswarm/sonobuoy-plugin/v5/pkg/capiutil"
 	"github.com/giantswarm/sonobuoy-plugin/v5/pkg/ctrlclient"
-	capi "sigs.k8s.io/cluster-api/api/v1alpha4"
-	capiconditions "sigs.k8s.io/cluster-api/util/conditions"
 )
 
 func Test_MachinePoolCR(t *testing.T) {
@@ -80,11 +83,53 @@ func Test_MachinePoolCR(t *testing.T) {
 		// Check if 'giantswarm.io/machine-pool' label is set
 		assert.LabelIsSet(t, &mp, label.MachinePool)
 
-		// Check if 'cluster.k8s.io/cluster-api-autoscaler-node-group-min-size' annotation is set
-		assert.AnnotationIsSet(t, &mp, annotation.NodePoolMinSize)
+		if mp.Spec.Template.Spec.InfrastructureRef.Kind == "AzureMachinePool" {
+			amp := v1alpha4.AzureMachinePool{}
+			err := cpCtrlClient.Get(ctx, client.ObjectKey{Namespace: mp.Spec.Template.Spec.InfrastructureRef.Namespace, Name: mp.Spec.Template.Spec.InfrastructureRef.Name}, &amp)
+			if err != nil {
+				t.Fatalf("unable to retrieve AzureMachinePool %q/%q: %v", mp.Spec.Template.Spec.InfrastructureRef.Namespace, mp.Spec.Template.Spec.InfrastructureRef.Name, err)
+			}
 
-		// Check if 'cluster.k8s.io/cluster-api-autoscaler-node-group-max-size' annotation is set
-		assert.AnnotationIsSet(t, &mp, annotation.NodePoolMaxSize)
+			var minReplicasString, maxReplicasString string
+			{
+				_, ok := amp.Spec.AdditionalTags["min"]
+				if ok {
+					// CAPZ cluster
+
+					minReplicasString = amp.Spec.AdditionalTags["min"]
+					maxReplicasString = amp.Spec.AdditionalTags["max"]
+				} else {
+					// GS clusters
+
+					// Check if 'cluster.k8s.io/cluster-api-autoscaler-node-group-min-size' annotation is set
+					assert.AnnotationIsSet(t, &mp, annotation.NodePoolMinSize)
+
+					// Check if 'cluster.k8s.io/cluster-api-autoscaler-node-group-max-size' annotation is set
+					assert.AnnotationIsSet(t, &mp, annotation.NodePoolMaxSize)
+
+					minReplicasString = mp.Annotations[annotation.NodePoolMinSize]
+					maxReplicasString = mp.Annotations[annotation.NodePoolMaxSize]
+				}
+			}
+
+			minReplicas, err := strconv.Atoi(minReplicasString)
+			if err != nil {
+				t.Fatalf("error converting additional tag 'min' to integer %v", err)
+			}
+
+			maxReplicas, err := strconv.Atoi(maxReplicasString)
+			if err != nil {
+				t.Fatalf("error converting additional tag 'max' to integer %v", err)
+			}
+
+			// Check if number of found replicas is within expected cluster autoscaler limits
+			if int(mp.Status.Replicas) < minReplicas {
+				t.Fatalf("specified min %d replicas, found %d", minReplicas, mp.Status.Replicas)
+			}
+			if int(mp.Status.Replicas) > maxReplicas {
+				t.Fatalf("specified max %d replicas, found %d", maxReplicas, mp.Status.Replicas)
+			}
+		}
 
 		// Wait for Ready condition to be True
 		capiutil.WaitForCondition(t, ctx, logger, &mp, capi.ReadyCondition, capiconditions.IsTrue, machinePoolGetter)
@@ -95,26 +140,6 @@ func Test_MachinePoolCR(t *testing.T) {
 		//
 		// Check Spec & Status
 		//
-
-		minReplicasString := mp.Annotations[annotation.NodePoolMinSize]
-		minReplicas, err := strconv.Atoi(minReplicasString)
-		if err != nil {
-			t.Fatalf("error converting annotation %q to integer %v", annotation.NodePoolMinSize, err)
-		}
-
-		maxReplicasString := mp.Annotations[annotation.NodePoolMaxSize]
-		maxReplicas, err := strconv.Atoi(maxReplicasString)
-		if err != nil {
-			t.Fatalf("error converting annotation %q to integer %v", annotation.NodePoolMaxSize, err)
-		}
-
-		// Check if number of found replicas is within expected cluster autoscaler limits
-		if int(mp.Status.Replicas) < minReplicas {
-			t.Fatalf("specified min %d replicas in annotation %q, found %d", minReplicas, annotation.NodePoolMinSize, mp.Status.Replicas)
-		}
-		if int(mp.Status.Replicas) > maxReplicas {
-			t.Fatalf("specified max %d replicas in annotation %q, found %d", maxReplicas, annotation.NodePoolMaxSize, mp.Status.Replicas)
-		}
 
 		// Check if all discovered replicas are ready
 		if mp.Status.Replicas != mp.Status.ReadyReplicas {
